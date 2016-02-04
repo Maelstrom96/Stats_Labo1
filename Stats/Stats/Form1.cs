f39b98b6b9dcff231090a5237b0e7ed9bb93d66b
mlussier17 <math.lussier@hotmail.com>
2016-02-04 11:38:18 -0500

    Read works (Write SOSO)

--- Stats/Stats/Form1.cs
+++ Stats/Stats/Form1.cs
@@ -62,13 +62,85 @@ namespace Stats
             Excel.Worksheet xlWorkSheet;
             Excel.Range range;
 
-            xlApp = new Excel.Application();
-            xlWorkBook = xlApp.Workbooks.Open(_originalFile, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
-            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-            range = xlWorkSheet.UsedRange;
-            //http://csharp.net-informations.com/excel/csharp-read-excel.htm
+            string str;
+            int rCnt = 0;
+            int cCnt = 0;
 
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open(oriFile, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            range = xlWorkSheet.UsedRange;
+
+            for (rCnt = 1; rCnt <= range.Rows.Count; rCnt++)
+            {
+                for (cCnt = 1; cCnt <= range.Columns.Count; cCnt++)
+                {
+                    str = (string)(range.Cells[rCnt, cCnt] as Excel.Range).Text;
+                    WriteFile(rCnt, cCnt, str);
+                }
+            }
+
+            xlWorkBook.Close(true, null, null);
+            xlApp.Quit();
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
         }
+
+        public void WriteFile(int rCnt, int cCnt, String str)
+        {
+            Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+
+            if (xlApp == null)
+            {
+                MessageBox.Show("Excel is not properly installed!!");
+                return;
+            }
+
+
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            xlWorkSheet.Cells[rCnt, cCnt] = str;
+
+            xlWorkBook.SaveAs(_savePath + @"\" + _saveFile, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+
+            MessageBox.Show("Fichier enregistré à " + _savePath + @"\" + _saveFile);
+        }
+
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Unable to release the Object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        } 
 
     }
 }
+
+
+
+//http://csharp.net-informations.com/excel/csharp-read-excel.htm
