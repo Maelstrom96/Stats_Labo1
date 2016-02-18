57867e9d8305bf6315c16c67c7f3f5066816daf9
Alexandre-Jacques St-Jacques <alexandre.st-jacques@groupegodin.com>
2016-02-18 08:35:54 -0500

    Ajout des fonctions pour les échantillons

---
+++ Stats/Stats/Utils.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stats
+{
+    static class Utils
+    {
+        public static List<List<string>> GetEchantillonAleatoire(List<List<string>> population, int tailleEchantillon)
+        {
+            List<List<string>> Echantillon = new List<List<string>>();
+            List<List<string>> pop = new List<List<string>>(population);
+            Random rnd = new Random();
+
+            // Erreurs
+            if (pop.Count == 0) throw new Exception("La population est vide.");
+            if (pop.Count < tailleEchantillon) throw new Exception("La taille d'échantillon demandé est plus grand que la population.");
+
+            for (int i = 0; i < tailleEchantillon; i++)
+            {
+                int tmpIndex = rnd.Next(pop.Count - 1);
+                Echantillon.Add(pop[tmpIndex]);
+                pop.RemoveAt(tmpIndex);
+            }
+
+            return Echantillon;
+        }
+
+        public static List<List<string>> GetEchantillonSystematique(List<List<string>> population, int tailleEchantillon)
+        {
+            int pas = (int)Math.Round((double)population.Count/tailleEchantillon);
+            List<List<string>> Echantillon = new List<List<string>>();
+            List<List<string>> pop = new List<List<string>>(population);
+            Random rnd = new Random();
+            int origin = rnd.Next(pop.Count - 1);
+            int start;
+
+            // Erreurs
+            if (pop.Count == 0) throw new Exception("La population est vide.");
+            if (pop.Count < tailleEchantillon) throw new Exception("La taille d'échantillon demandé est plus grand que la population.");
+
+            // Go to first data
+            for (start = origin; start >= 0; start = start - pas) ;
+
+            for (int i = start ; i < pop.Count; i = i + pas )
+            {
+                Echantillon.Add(pop.ElementAt(i));
+            }
+
+            return Echantillon;
+        }
+    }
+}
